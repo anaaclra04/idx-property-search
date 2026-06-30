4a011d3fab8f8b5fc4acc5c8a282f9a236fab60d
Ana Clara Machado Goncalves <anaaclra04@gmail.com>
2026-06-30 12:03:00 -0400

    db directory update

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const db = require('./db'); 
+const db = require('./src/db/db'); 
 
 const app = express();
 
